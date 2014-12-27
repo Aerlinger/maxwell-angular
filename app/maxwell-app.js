3c75f3022226ed906eb9a851e14204723501a575
Anthony Erlinger <aerlinger@gmail.com>
2014-12-27 10:43:05 -0800

    Add abstract routing w/ state provider in 'Maxwell' module

--- app/maxwell-app.js
+++ app/maxwell-app.js
@@ -2,4 +2,10 @@ angular.module('Maxwell', [
     'ui.router',
     'components',
     'circuit'
-]);
+]).config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('default', {
+        url: "/",
+        abstract: true
+    });
+    $urlRouterProvider.otherwise('/');
+});
